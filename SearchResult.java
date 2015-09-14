8054287c24e2c783fc650251b90381a79fb8cbde
bala1808 <bala.gunner@gmail.com>
2015-09-14 12:13:04 -0700

    Create SearchResult.java

---
+++ SearchResult.java
@@ -0,0 +1,6 @@
+//SearchResult class is a helper class used to store and return result of the searchTrie function.
+	
+public class SearchResult {
+		int vertex;					// Indicates the vertex at which the searched IP address should be present.
+		String longestMatch = null;	// Indicates the longest string matched with the given IP address. 
+}
